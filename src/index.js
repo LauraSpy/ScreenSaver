8bae3a2e4b68cc8e399dab9fd5df886c5b97f05f
LauraSpy <137756546+LauraSpy@users.noreply.github.com>
2024-07-29 15:39:24 +0200

    création des routes et des dossiers pour structures de base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DetailFilm from './pages/detail/detail.jsx';
+import UserProfilePage from './pages/profil/profil.jsx';
+import ContactPage from './pages/contact/contact.jsx';
+import filterFilm from './pages/search_page/searchPage.jsx';
+import Home from './pages/homepage.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="/" element={<Home />} />
+          <Route path="/film/:id" element={<DetailFilm />} />
+          <Route path="/film/filter" element={<filterFilm />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+        </ Route >
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
