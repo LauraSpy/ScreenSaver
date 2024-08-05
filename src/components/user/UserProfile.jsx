16487ad2f057316f29e45913e45c4ebed8ba7f18
LauraSpy <137756546+LauraSpy@users.noreply.github.com>
2024-08-05 19:56:49 +0200

    ajout des images pour le compte profile

--- src/components/user/UserProfile.jsx
+++ src/components/user/UserProfile.jsx
@@ -1,14 +1,27 @@
 // src/components/UserProfile.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateProfile } from '../../redux/authSlice';
+import defaultBanner from '../../images/banner/banner-default.png';
+import defaultAvatar from '../../images/avatar/avatar-default.png';
+import banner1 from '../../images/banner/banner1.jpg';
+import banner2 from '../../images/banner/banner2.jpg';
+import avatar1 from '../../images/avatar/avatar1.jpg';
+import avatar2 from '../../images/avatar/avatar2.jpg';
+import s from './styles.module.css';
 
 const UserProfile = () => {
   const user = useSelector((state) => state.auth.user);
   const dispatch = useDispatch();
+  const [bannerOptions] = useState([defaultBanner, banner1, banner2]);
+  const [avatarOptions] = useState([defaultAvatar, avatar1, avatar2]);
 
-  const handleImageChange = (type, imageUrl) => {
-    dispatch(updateProfile({ [type]: imageUrl }));
+  const handleImageChange = (type) => {
+    const options = type === 'banner' ? bannerOptions : avatarOptions;
+    const currentIndex = options.indexOf(user[type] || options[0]);
+    const nextIndex = (currentIndex + 1) % options.length;
+    const newImage = options[nextIndex];
+    dispatch(updateProfile({ [type]: newImage }));
   };
 
   // Simuler des données de progression
@@ -23,14 +36,14 @@ const UserProfile = () => {
   return (
     <div>
       <div>
-        <img src={user.banner || 'default-banner.jpg'} alt="Bannière" />
-        <button onClick={() => handleImageChange('banner', 'new-banner-url.jpg')}>
+        <img src={user.banner || defaultBanner} alt="Bannière" style={{width: '100%', height: '55vh', objectFit: 'cover'}} />
+        <button onClick={() => handleImageChange('banner')}>
           Changer la bannière
         </button>
       </div>
       <div>
-        <img src={user.avatar || 'default-avatar.png'} alt="Avatar" />
-        <button onClick={() => handleImageChange('avatar', 'new-avatar-url.jpg')}>
+        <img src={user.avatar || defaultAvatar} alt="Avatar" style={{width: '100px', height: '100px', objectFit: 'cover', borderRadius: '50%'}} />
+        <button onClick={() => handleImageChange('avatar')}>
           Changer l'avatar
         </button>
       </div>
--- src/pages/profil/profil.jsx
+++ src/pages/profil/profil.jsx
@@ -4,7 +4,6 @@ import UserProfile from "../../components/user/UserProfile";
 const UserProfilePage = () => {
     return (
         <div className="profile">
-            <h1>Profile</h1>
             <UserProfile />
         </div>
     );
