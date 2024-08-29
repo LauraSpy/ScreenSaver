3d8e03803a58af6088ce89749f5cff803b7d9357
LauraSpy <137756546+LauraSpy@users.noreply.github.com>
2024-08-29 12:25:44 +0200

    modif logique du menu pour fermer les onglets de menu dans l'overlay

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,87 +3,78 @@ import { useNavigate } from 'react-router-dom';
 import s from './styles.module.css';
 import arrowDown from '../../images/buttons/bottom.svg';
 
-// Composant NavBar pour la navigation principale
-// isOverlay est utilisé pour le style en mode responsive sur mobile
-const NavBar = ({ isOverlay = false, closeMenuOverlay  }) => {
-    // État pour gérer l'ouverture des menus déroulants
+const NavBar = ({ isOverlay = false, closeMenuOverlay }) => {
     const [dropdownOpen, setDropdownOpen] = useState(null);
     const navigate = useNavigate();
 
-    // Gestion de l'ouverture du menu au survol
+    const toggleDropdown = (menu) => {
+        if (isOverlay) {
+            setDropdownOpen(prevState => prevState === menu ? null : menu);
+        }
+    };
+
     const handleMouseEnter = (menu) => {
-        setDropdownOpen(menu);
+        if (!isOverlay) {
+            setDropdownOpen(menu);
+        }
     };
 
-    // Fermeture du menu lorsque la souris quitte la zone
     const handleMouseLeave = () => {
-        setDropdownOpen(null);
+        if (!isOverlay) {
+            setDropdownOpen(null);
+        }
     };
 
-    // Pour gérer, au clic d'un lien, la remise à l'état initial du menu burger
     const handleNavigation = (path) => {
         navigate(path);
-        // on fait appel au prop 
         if (isOverlay && closeMenuOverlay) {
             closeMenuOverlay();
         }
     };
 
+    const renderMenu = (menuName, items) => (
+        <li
+            onMouseEnter={() => handleMouseEnter(menuName)}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => toggleDropdown(menuName)}
+        >
+            <button>
+                {menuName} <img src={arrowDown} alt="arrow down" style={{ marginLeft:'15%' }} />
+            </button>
+            {dropdownOpen === menuName && (
+                <ul className={s.dropdown}>
+                    {items.map((item, index) => (
+                        <li key={index} onClick={() => handleNavigation(item.path)}>{item.label}</li>
+                    ))}
+                </ul>
+            )}
+        </li>
+    );
+
+    const menuItems = {
+        Films: [
+            { label: 'Films populaires', path: '/list/films/popular' },
+            { label: 'Films les mieux notés', path: '/list/films/top-rated' },
+            { label: 'Films à l\'affiche', path: '/list/films/now-playing' },
+            { label: 'Films à voir', path: '/list/films/to-watch' },
+            { label: 'Films déjà vus', path: '/list/films/already-watched' }
+        ],
+        Séries: [
+            { label: 'Séries du moment', path: '/list/series/current' },
+            { label: 'Séries populaires', path: '/list/series/popular' },
+            { label: 'Séries les mieux notées', path: '/list/series/top-rated' },
+            { label: 'Séries à voir', path: '/list/series/to-watch' },
+            { label: 'Séries déjà vues', path: '/list/series/already-watched' }
+        ],
+        'Mes listes': [
+            { label: 'Mes Favoris', path: '/my-list' }
+        ]
+    };
+
     return (
         <nav className={isOverlay ? s.navOverlay : ''}>
             <ul className={`${s.navLinks} ${isOverlay ? s.navLinksOverlay : ''}`}>
-                {/* Menu Films */}
-                <li
-                    onMouseEnter={() => handleMouseEnter('films')}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    <button>
-                        Films <img src={arrowDown} alt="arrow down" style={{ marginLeft:'15%' }} />
-                    </button>
-                    {dropdownOpen === 'films' && (
-                        <ul className={s.dropdown}>
-                            <li onClick={() => handleNavigation('/list/films/popular')}>Films populaires</li>
-                            <li onClick={() => handleNavigation('/list/films/top-rated')}>Films les mieux notés</li>
-                            <li onClick={() => handleNavigation('/list/films/now-playing')}>Films à l'affiche</li>
-                            <li onClick={() => handleNavigation('/list/films/to-watch')}>Films à voir</li>
-                            <li onClick={() => handleNavigation('/list/films/already-watched')}>Films déjà vues</li>
-                        </ul>
-                    )}
-                </li>
-
-                {/* Menu Séries */}
-                <li 
-                    onMouseEnter={() => handleMouseEnter('series')}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    <button>
-                        Séries <img src={arrowDown} alt="arrow down" style={{ marginLeft:'15%' }} />
-                    </button>
-                    {dropdownOpen === 'series' && (
-                        <ul className={s.dropdown}>
-                            <li onClick={() => handleNavigation('/list/series/current')}>Séries du moment</li>
-                            <li onClick={() => handleNavigation('/list/series/popular')}>Séries populaires</li>
-                            <li onClick={() => handleNavigation('/list/series/top-rated')}>Séries les mieux notées</li>
-                            <li onClick={() => handleNavigation('/list/series/to-watch')}>Séries à voir</li>
-                            <li onClick={() => handleNavigation('/list/series/already-watched')}>Séries déjà vues</li>
-                        </ul>
-                    )}
-                </li>
-
-                {/* Menu Mes Favoris */}
-                <li
-                    onMouseEnter={() => handleMouseEnter('myList')}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    <button>
-                        Mes listes <img src={arrowDown} alt="arrow down" style={{ marginLeft:'15%' }} />
-                    </button>
-                    {dropdownOpen === 'myList' && (
-                        <ul className={s.dropdown}>
-                            <li onClick={() => handleNavigation('/my-list')}>Mes Favoris</li>
-                        </ul>
-                    )}
-                </li>
+                {Object.entries(menuItems).map(([menuName, items]) => renderMenu(menuName, items))}
             </ul>
         </nav>
     );
